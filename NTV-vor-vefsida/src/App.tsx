d100a515ff13b6e4de69998be3f95084d9aec1b0
Inga Beck <ingambeck@gmail.com>
2026-02-19 20:23:44 +0000

    create initial files

---
+++ NTV-vor-vefsida/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [myName, setMyName] = useState("Inga");
+  const [submit, setSubmit] = useState("");
+
+  function handleChange(e) {
+    setMyName(e.target.value);
+  }
+  function submitted(event) {
+    event.preventDefault();
+    setSubmit("submitted");
+  }
+  function changeColor() {}
+
+  return (
+    <>
+      <h2>Hello World!</h2>
+      <div>{myName}</div>
+      <form onSubmit={submitted}>
+        <input
+          type="text"
+          placeholder="enter your name"
+          onChange={handleChange}
+        />
+        <input type="email" placeholder="enter your email" />
+        <input type="submit" name="" id="" />
+        <p>{submit}</p>
+      </form>
+      <div className="stuff" style={{ backgroundColor: "red" }}>
+        {/* ath hér að breyta background á div með því að smella á takka */}
+        <button onClick={changeColor}>Party</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
