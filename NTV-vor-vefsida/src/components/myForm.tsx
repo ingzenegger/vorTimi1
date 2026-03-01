642da30bd9cfd25c9678dadc40a28032fbc91b03
Inga Beck <ingambeck@gmail.com>
2026-03-01 11:26:35 +0000

    myForm functionality added

--- NTV-vor-vefsida/src/components/myForm.tsx
+++ NTV-vor-vefsida/src/components/myForm.tsx
@@ -39,8 +39,15 @@ import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { useState } from "react";
 
 export default function MyForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState(0);
+  const [selected, setSelected] = useState("");
+  const [approve, setApprove] = useState(true);
   const items = [
     { label: "Life of Brian", value: "brian" },
     { label: "Blackadder Season 3", value: "blackadder" },
@@ -48,6 +55,24 @@ export default function MyForm() {
     { label: "Spaceballs", value: "spaceballs" },
     { label: "Ghostbusters", value: "ghostbusters" },
   ];
+
+  const onClick = () => {
+    alert(
+      approve
+        ? `Hello ${firstName} ${lastName}, your email(${email}) and mobile number (${mobileNumber}) have now been added to a random trivia mailing list relating to ${selected} `
+        : `Hello ${firstName} ${lastName}, by choosing "no" you have opted out of recieving random trivia to your email (${email}) and mobile number (${mobileNumber}) relating to ${selected} `,
+    );
+  };
+  //perhaps not necessary as the button automatically refreshes the page...
+  const resetStates = () => {
+    // event.preventDefault();
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMobileNumber(0);
+    setSelected("");
+    setApprove(true);
+  };
 
   return (
     <div className="w-full max-w-md">
@@ -60,27 +85,43 @@ export default function MyForm() {
             <FieldGroup>
               <Field>
                 {/* <FieldLabel>First name:</FieldLabel> */}
-                <Input type="text" placeholder="First name" />
+                <Input
+                  type="text"
+                  placeholder="First name"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
               </Field>
               <Field>
                 {/* <FieldLabel>Last name:</FieldLabel> */}
-                <Input type="text" placeholder="Last name" />
+                <Input
+                  type="text"
+                  placeholder="Last name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
               </Field>
               <Field>
                 {/* <FieldLabel>Email:</FieldLabel> */}
-                <Input type="email" placeholder="Email" />
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </Field>
               <Field>
                 {/* <FieldLabel>Mobile number:</FieldLabel> */}
-                <Input type="number" placeholder="Mobile number" />
+                <Input
+                  type="number"
+                  placeholder="Mobile number"
+                  onChange={(e) => setMobileNumber(Number(e.target.value))}
+                />
               </Field>
-              <Select>
+              <Select onValueChange={(value) => setSelected(value)}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select..." />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectLabel>Greatest hits</SelectLabel>
+                    <SelectLabel>The GOAT?</SelectLabel>
                     {items.map((item) => (
                       <SelectItem key={item.value} value={item.label}>
                         {item.label}
@@ -89,25 +130,30 @@ export default function MyForm() {
                   </SelectGroup>
                 </SelectContent>
               </Select>
-              <RadioGroup className="flex">
+              <RadioGroup
+                className="flex"
+                onValueChange={(value) => setApprove(value === "true")}
+                value={approve.toString()}
+              >
                 <div className="flex items-center gap-3">
-                  <RadioGroupItem
-                    value="option-one"
-                    id="option-one"
-                  ></RadioGroupItem>
+                  <RadioGroupItem value="true" id="option-one"></RadioGroupItem>
                   <Label htmlFor="option-one">Yes</Label>
                 </div>
                 <div className="flex items-center gap-3">
                   <RadioGroupItem
-                    value="option two"
+                    value="false"
                     id="option-two"
                   ></RadioGroupItem>
                   <Label htmlFor="option-two">No</Label>
                 </div>
               </RadioGroup>
-              <Button className="w-full">Submit</Button>
+              <Button onClick={onClick} className="w-full">
+                Submit
+              </Button>
               <Separator title="or" />
-              <Button className="w-full">Edit</Button>
+              <Button className="w-full" onClick={resetStates}>
+                Clear
+              </Button>
             </FieldGroup>
           </form>
         </CardContent>
